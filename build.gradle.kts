7bd7a88e569b9bd8995cf8233b79cf2c70e8c696
Alexandre Boyer <nggalien@gmail.com>
2022-09-22 13:39:41 +0200

    Add publish workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.7.10"
     jacoco
+    `maven-publish`
 }
 
 group = "net.ngg"
@@ -10,6 +11,19 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/ngglien/ddd-kotlin")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
 
 dependencies {
